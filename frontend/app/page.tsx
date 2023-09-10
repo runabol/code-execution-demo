7668e324a09188830067ceb02714361ee7cb1e55
Arik Cohen <me@arikcohen.com>
2023-09-10 17:31:06 -0400

    finished frontend

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -21,17 +21,18 @@ export default function Home() {
                 onChange={(ev) => setLanguage(ev.target.value)}
               >
                 <option value="bash">Bash</option>
-                <option value="python3">Python</option>
-                <option value="golang">Golang</option>
+                <option value="python">Python</option>
+                <option value="go">Golang</option>
               </select>
               <button
                 type="button"
                 className="rounded bg-indigo-600 px-3 py-2 text-md font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "
                 onClick={() => {
+                  setOutput("Executing...");
                   fetch("/api/execute", {
                     method: "POST",
                     body: JSON.stringify({
-                      langauge: language,
+                      language: language,
                       code: code,
                     }),
                     headers: {
@@ -54,14 +55,14 @@ export default function Home() {
           </div>
           <Editor
             height="25rem"
-            language="bash"
+            language={language}
             theme="vs-dark"
             className="max-w-5xl"
             value={code}
             onChange={(value) => setCode(value || "")}
           />
           <div className="overflow-hidden bg-white shadow sm:rounded-lg">
-            <div className="px-4 py-5 sm:p-6">{output}</div>
+            <div className="px-4 py-5 sm:p-6 whitespace-pre">{output}</div>
           </div>
         </div>
       </div>
